429ee2322d5997e6e878b24db7055fc16ad2321a
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-03-03 00:24:07 +0545

    some more thoughts about the giga seconds here

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -2,5 +2,6 @@ use time::PrimitiveDateTime as DateTime;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
+    start.replace_second(1000000000);
     todo!("What time is a gigasecond later than {start}");
 }
